0de7f469ff349bf05380d5ce96bd3b6d798af55f
AlexandreDev <eztalenda@gmail.com>
2022-05-19 02:38:10 -0300

    feat(main): ajustes de funcionalidades de delete e rotinas com padronizacao de request

--- src/components/FuncionarioView.js
+++ src/components/FuncionarioView.js
@@ -8,6 +8,8 @@ import CardContent from '@mui/material/CardContent'
 import Card from '@mui/material/Card'
 import { toast } from 'react-toastify'
 
+import api from '../services/api'
+
 const logo = require('../images/profile.png')
 
 const mockUserProfile = {
@@ -15,26 +17,22 @@ const mockUserProfile = {
 }
 
 const FuncionarioView = () => {
-  const [getFuncionarioData, setFuncionarioData] = useState([])
+  const [userToEdit, setUserToEdit] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
 
   const { id } = useParams('')
 
   const navigate = useNavigate()
 
   const getFuncionario = async () => {
-    const res = await fetch(`/get-funcionario/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-
-    const data = await res.json()
-
-    if (res.status === 500 || !data) {
-      toast.error('Não foi possivel obter o funcionário!!')
-    } else {
-      setFuncionarioData(data)
+    setIsLoading(true)
+    try {
+      const res = await api.get(`/get-funcionario/${id}`)
+      setUserToEdit(res.data)
+    } catch (err) {
+      throw new Error(err)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -44,36 +42,36 @@ const FuncionarioView = () => {
   }, [])
 
   const handleDelete = async (id) => {
-    const res2 = await fetch(`/delete-funcionario/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    const deleteFuncionario = await res2.json()
+    setIsLoading(true)
+    try {
+      const res = await api.delete(`/delete-funcionario/${id}`)
 
-    if (res2.status === 500 || !deleteFuncionario) {
-      toast.error('Não foi possivel Deletar o funcionário!!')
-    } else {
       toast.success('Funcionario Deletado com Sucesso!')
+      getFuncionario(res.data)
       navigate('/')
+    } catch (err) {
+      throw new Error(err)
+    } finally {
+      setIsLoading(false)
     }
   }
 
-  return (
+  return isLoading ? (
+    <div>Está carregando...</div>
+  ) : (
     <div className="container mt-3 ">
       <h1 style={{ fontWeight: 400 }}>{`Bem vindo ${mockUserProfile.username}`}</h1>
 
       <Card sx={{ maxWidth: 600 }}>
         <CardContent>
           <div className="add_btn">
-            <NavLink to={`/edit-funcionario/${getFuncionarioData._id}`}>
+            <NavLink to={`/edit-funcionario/${userToEdit._id}`}>
               <button className="btn btn-primary mx-2">
                 <CreateIcon />
               </button>
             </NavLink>
 
-            <button className="btn btn-danger" onClick={() => handleDelete(getFuncionarioData._id)}>
+            <button className="btn btn-danger" onClick={() => handleDelete(userToEdit._id)}>
               <DeleteOutlineIcon />
             </button>
           </div>
@@ -81,21 +79,21 @@ const FuncionarioView = () => {
             <div className="left_view col-lg-6 col-md-6 col-12">
               <img src={logo} style={{ width: 50 }} alt="profile" />
               <h3 className="mt-3">
-                Nome: <span style={{ fontWeight: 400 }}>{getFuncionarioData.nome}</span>
+                Nome: <span style={{ fontWeight: 400 }}>{userToEdit.nome}</span>
               </h3>
               <h3 className="mt-3">
-                Sobrenome: <span style={{ fontWeight: 400 }}>{getFuncionarioData.sobrenome}</span>
+                Sobrenome: <span style={{ fontWeight: 400 }}>{userToEdit.sobrenome}</span>
               </h3>
             </div>
 
             <div className="right_view col-lg-6 col-md-6 col-12">
               <p className="mt-5">
                 <MailOutlineIcon />
-                Email: <span>{getFuncionarioData.email}</span>
+                Email: <span>{userToEdit.email}</span>
               </p>
               <p className="mt-3">
                 <CreditCardIcon />
-                Número NIS: <span>{getFuncionarioData.nnis}</span>
+                Número NIS: <span>{userToEdit.nnis}</span>
               </p>
             </div>
           </div>
